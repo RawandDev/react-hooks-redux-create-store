bb70876ce99b1451250e79955735812194079c7a
RawandDev <rawand.200091384037@gmail.com>
2021-08-22 23:49:43 +0300

    done

--- src/createStore.js
+++ src/createStore.js
@@ -1,28 +1,51 @@
-let state;
+const ACTIONS = {
+  INCREASE_COUNTER: "counter/increment",
+  DECREASE_COUNTER: "counter/decrement",
+};
+
+function createStore(reducer) {
+  let state;
+
+  function dispatch(action) {
+    state = reducer(state, action);
+    render();
+  }
+
+  function getState() {
+    return state;
+  }
+
+  return { dispatch, getState };
+}
 
 function reducer(state = { count: 0 }, action) {
   switch (action.type) {
-    case "counter/increment":
+    case ACTIONS.INCREASE_COUNTER:
       return { count: state.count + 1 };
+
+    case ACTIONS.DECREASE_COUNTER:
+      return { count: state.count - 1 };
 
     default:
       return state;
   }
 }
 
-function dispatch(action) {
-  state = reducer(state, action);
-  render();
-}
-
 function render() {
   let container = document.getElementById("container");
-  container.textContent = state.count;
+  container.textContent = store.getState().count;
 }
 
-dispatch({ type: "@@INIT" });
+let store = createStore(reducer);
+
+store.dispatch({ type: "@@INIT" });
 let button = document.getElementById("button");
+let button2 = document.getElementById("button2");
 
 button.addEventListener("click", function () {
-  dispatch({ type: "counter/increment" });
+  store.dispatch({ type: ACTIONS.INCREASE_COUNTER });
+});
+
+button2.addEventListener("click", function () {
+  store.dispatch({ type: ACTIONS.DECREASE_COUNTER });
 });
